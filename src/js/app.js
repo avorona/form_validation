e488a8d0de2f11bacc1c4183ae20cec963ab2913
AVorona <vitalii.parshikov@gmail.com>
2017-10-29 00:52:50 +0300

    error and succeed handling

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 var form = document.getElementById('userForm');
-var modalStatus = document.querySelector('.js-modal-status');
+
+// clearInputStatus();
+// clearMessageStatus();
 
 form.addEventListener('submit', function() {
   event.preventDefault();
@@ -14,33 +16,23 @@ form.addEventListener('submit', function() {
 
 function validateform(formToSubmit) {
 
-
   var form = formToSubmit;
 
   // clear styling
 
-  var inputElements=document.querySelectorAll('.js-form-input');
-
-  inputElements.forEach(function(el) {
-    el.classList.remove('is-invalid');
-  });
-
-
-
-
   // define regexp
- 	var regexp = {
+  var regexp = {
 
- 		digitsRegExp : new RegExp('^[0-9]+$'),
- 		stringRegExp : new RegExp('^[a-zA-Z]+$'),
- 		emailRegExp  : new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"),
+    digitsRegExp: new RegExp('^[0-9]+$'),
+    stringRegExp: new RegExp('^[a-zA-Z]+$'),
+    emailRegExp: new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
 
- 		mediumPassRegExp : new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'),
- 		  // contains six characters or more and has
-  		// at least one lowercase and one uppercase alphabetical character or has at least one lowercase
-  		// and one numeric character or has at least one uppercase and one numeric character.  
+    mediumPassRegExp: new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'),
+    // contains six characters or more and has
+    // at least one lowercase and one uppercase alphabetical character or has at least one lowercase
+    // and one numeric character or has at least one uppercase and one numeric character.  
 
- 	};
+  };
 
 
 
@@ -49,32 +41,32 @@ function validateform(formToSubmit) {
   var inputsArray = [
 
     {
-    	name : form.elements['name'],
-    	pattern: regexp.stringRegExp
+      name: form.elements['name'],
+      pattern: regexp.stringRegExp
     },
     {
-    	name : form.elements['surname'],
-    	pattern: regexp.stringRegExp
+      name: form.elements['surname'],
+      pattern: regexp.stringRegExp
     },
     {
-    	name : form.elements['phone'],
-    	pattern: regexp.digitsRegExp
+      name: form.elements['phone'],
+      pattern: regexp.digitsRegExp
     },
     {
-    	name : form.elements['email'],
-    	pattern: regexp.emailRegExp
+      name: form.elements['email'],
+      pattern: regexp.emailRegExp
     },
 
     {
-    	 name : form.elements['password'],
-    	 pattern: regexp.mediumPassRegExp
+      name: form.elements['password'],
+      pattern: regexp.mediumPassRegExp
     }
 
 
   ];
 
 
-  var booleanArray = inputsArray.map(function(el)	{
+  var booleanArray = inputsArray.map(function(el) {
 
     if (validateInput(form, el.name, el.pattern)) {
 
@@ -97,199 +89,184 @@ function validateform(formToSubmit) {
     return false;
   } else {
 
+  	// showCongrats();
     form.submit();
 
   }
 
 
-
-  // if (validateInput(form, inputs.name, stringRegExp)) {
-  //   formToSubmit.submit();
-  // } else {
-  //   // console.log(validateInput(form, nameInput, stringRegExp ));
-  //   return false;
-  // }
-
-  // if (validateInput(form, inputs.surname, stringRegExp)) {
-  //   formToSubmit.submit();
-  // } else {
-  //   // console.log(validateInput(form, nameInput, stringRegExp ));
-  //   return false;
-  // }
-
-
-  // if (validateInput(form, inputs.phone, digitsRegExp	)) {
-  //   formToSubmit.submit();
-  // } else {
-  //   // console.log(validateInput(form, nameInput, stringRegExp ));
-  //   return false;
-  // }
+}
 
 
 
-  // if (validateInput(form, inputs.email, emailRegExp)) {
-  //   formToSubmit.submit();
-  // } else {
-  //   // console.log(validateInput(form, nameInput, stringRegExp ));
-  //   return false;
-  // }
+function validateInput(form, input, regexp) {
 
 
-  // if (validateInput(form, inputs.pass, mediumPassRegExp)) {
-  //   formToSubmit.submit();
-  // } else {
-  //   // console.log(validateInput(form, nameInput, stringRegExp ));
-  //   return false;
-  // }
+  if ((!validateLength(form, input)) || (!validateData(form, input, regexp))) {
+    // console.log(validateLength(form, input), validateData(form, input, regexp));
 
+    showError(input);
 
+    return false;
+  } else {
+    showStatus(input);
 
-
-
-  // validateInput(form, surnameInput, stringRegExp);
-  // validateInput(form, phoneInput, digitsRegExp );
-  // validateInput(form, emailInput, emailRegExp);
-  // validateInput(form, passInput, mediumPassRegExp);
-
-
-
-
-
-
-  // validate length
-
-  function validateInput(form, input, regexp) {
-    console.log(form, input);
-
-    if ((!validateLength(form, input)) || (!validateData(form, input, regexp))) {
-      console.log(validateLength(form, input), validateData(form, input, regexp));
-
-      showError(form, input);
-
-      return false;
-    }
     return true;
   }
 
 
+}
 
 
 
 
-  function validateLength(formOfInputs, typeOfInput) {
+function validateLength(formOfInputs, typeOfInput) {
 
-  	var inputName=typeOfInput.name;
-    var inputValue = typeOfInput.value;
-    var inputValueLength = inputValue.length;
-   
-    if (inputName === 'phone') {
-      if ((inputValueLength >= 10) && (inputValueLength <= 12 )) {
-        // console.log(typeOfInput.value, inputValueLength, 'it has length');
-        return true;
-      } else {
-      	 
-        return false;
-      }
-    } else if (inputName === 'password') {
-      if ((inputValueLength >= 6) && (inputValueLength <= 100)) {
-        // console.log(typeOfInput.value, inputValueLength, 'it has length');
-        return true;
-      } else {
-      	// console.log(false);
-        return false;
-      }
+  var inputName = typeOfInput.name;
+  var inputValue = typeOfInput.value;
+  var inputValueLength = inputValue.length;
+
+  if (inputName === 'phone') {
+    if ((inputValueLength >= 10) && (inputValueLength <= 12)) {
+      // console.log(typeOfInput.value, inputValueLength, 'it has length');
+      return true;
     } else {
 
-      if ((typeOfInput !== '') && (inputValueLength >= 2)) {
-        // console.log(typeOfInput.value, inputValueLength,'it has length');
-        return true;
-      } else {
-      	// console.log(false);
-        return false;
-      }
+      return false;
     }
-
-
-  }
-
-
-  function validateData(formOfInputs, typeOfInput, regexp) {
-    var inputValue = typeOfInput.value;
-
-    if (regexp.test(inputValue)) {
+  } else if (inputName === 'password') {
+    if ((inputValueLength >= 6) && (inputValueLength <= 100)) {
+      // console.log(typeOfInput.value, inputValueLength, 'it has length');
       return true;
+    } else {
+      // console.log(false);
+      return false;
     }
+  } else {
 
-
-
-    return false;
-
+    if ((typeOfInput !== '') && (inputValueLength >= 2)) {
+      // console.log(typeOfInput.value, inputValueLength,'it has length');
+      return true;
+    } else {
+      // console.log(false);
+      return false;
+    }
   }
 
-
-
-
-
-
-
-
-
-  //   if ((form.elements['name'].value === '') || (form.elements['name'].value.length < 2)) {
-  //    	showError(form,'name');
-  //     return false;
-  //   }
-
-  //   if ((form.elements['surname'].value === '') || (form.elements['surname'].value.length < 2)) {
-  //    	showError(fieldArray,'surname');
-  //     return false;
-  //   }
-
-
-
-  //   if (form.elements['phone'].value === '') {
-  //     showError(fieldArray,'phone');
-  // 	    return false;
-  //   }
-
-
-  //   form.elements['phone'].value = form.elements['phone'].value.replace(digitsRegExp, '');
-
-  //   if (form.elements['phone'].value.length < 10) {
-  //     showError(fieldArray,'phone');
-  // 	    return false;
-  //   }
-
-
-  //   if (form.elements['phone'].value.length > 11) {
-  //     showError(fieldArray,'phone');
-  // 	    return false;
-  //   }
-
-  //   form.elements['email'].value = form.elements['email'].value.replace(emailRegExp, '');
-
-  //   if (form.elements['email'].value === '') {
-  //   	showError(fieldArray, email);
-  //   	return false;
-  //   }
-
-  //   // form.submit();
-  // }
-
-
-
-  function showError(fieldArray, field) {
-
-    var element = field;
-
-    element.classList.add('is-invalid');
-
-    console.log('showError');
-
-
-  }
-
-
-
-  // formToSubmit.submit();
 
 }
+
+
+
+function validateData(formOfInputs, typeOfInput, regexp) {
+  var inputValue = typeOfInput.value;
+
+  if (regexp.test(inputValue)) {
+    return true;
+  }
+
+  return false;
+
+}
+
+
+
+
+
+function showError(element) {
+
+  var field = element;
+  element.classList.remove('is-valid');
+  element.classList.add('is-invalid');
+
+  var errorMessages = document.querySelectorAll('.js-error-message');
+
+  errorMessages.forEach(function(el) {
+
+    var dataAttrValue = el.getAttribute('data-messagetype');
+
+    if (dataAttrValue === field.name) {
+      el.classList.add('is-for-error');
+    }
+
+  });
+}
+
+
+
+
+function showStatus(currentElement) {
+
+  changeInputStatus(currentElement);
+  // currentElement.classList.remove('is-invalid');
+  currentElement.classList.add('is-valid');
+
+
+
+};
+
+
+
+
+
+function changeInputStatus(input = 'false') {
+
+  if (input === 'false') {
+
+    var inputElements = document.querySelectorAll('.js-form-input');
+
+    inputElements.forEach(function(el) {
+
+      el.classList.remove('is-invalid');
+
+    });
+
+  }
+
+  input.classList.remove('is-invalid');
+
+
+  changeMessageStatus(input, true);
+
+}
+
+
+
+
+function changeMessageStatus(input, messageBox = false) {
+
+  var errorMessages = document.querySelectorAll('.js-error-message');
+
+  errorMessages.forEach(function(el) {
+
+
+    if (messageBox === false) {
+
+      el.classList.remove('is-for-error');
+
+    } else {
+
+
+      var dataAttrValue = el.getAttribute('data-messagetype');
+
+      if (dataAttrValue === input.name) {
+        console.log(true);
+        el.classList.remove('is-for-error');
+      }
+
+    }
+
+
+
+  });
+
+
+
+}
+
+
+// function showCongrats() {
+
+// 	var 
+	
+// }
